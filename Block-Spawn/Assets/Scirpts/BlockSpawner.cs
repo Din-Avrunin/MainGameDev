58858eaabf1a290158dbcef996956acf062397b0
Noam Domovich <ndomovich@gmail.com>
2020-05-05 19:06:42 +0300

    Falling Blocks
    Hw - part 3

---
+++ Block-Spawn/Assets/Scirpts/BlockSpawner.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+
+public class BlockSpawner : MonoBehaviour
+{
+    public Transform[] spawnPoints;
+
+    public GameObject blockPrefab;
+
+    public float timeBetweenWaves = 1f;
+
+    private float timeToSpawn = 2f;
+    
+    // Start is called before the first frame update
+    void Update()
+    {
+
+        if(Time.time >= timeToSpawn)
+        {
+            SpawnBlocks();
+            timeToSpawn = Time.time + timeBetweenWaves;
+               
+
+        }
+        
+    }
+
+
+    void SpawnBlocks ()
+    {
+        int randomIdx = Random.Range(0, spawnPoints.Length);
+
+        for (int i = 0; i < spawnPoints.Length; i++)
+        {
+            if (randomIdx != i)
+            {
+                Instantiate(blockPrefab, spawnPoints[i].position, Quaternion.identity);
+            }
+        }
+    }
+   
+}
---
+++ Block-Spawn/Assets/Scirpts/Player.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using UnityEngine;
+using UnityEngine.UIElements;
+
+public class Player : MonoBehaviour
+{
+    public float speed = 15f;
+
+    private Rigidbody2D rb;
+    private float mapWidth = 4f;
+
+    private void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    private void FixedUpdate()
+    {
+        float x = Input.GetAxis("Horizontal") *Time.deltaTime * speed;
+
+        Vector2 newPosition = rb.position + Vector2.right * x;
+
+        newPosition.x = Mathf.Clamp(newPosition.x, -mapWidth, mapWidth);
+
+        rb.MovePosition(newPosition);
+
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        FindObjectOfType<GameManger>().EndGame();
+
+    }
+
+}
