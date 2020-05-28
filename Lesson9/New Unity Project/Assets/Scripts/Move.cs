37f40f9ab37a4a7dd08b7a08ef7d5acb32bdf44a
Denis <aripash@gmail.com>
2020-05-28 09:58:57 +0300

    lesson 9

---
+++ Lesson9/New Unity Project/Assets/Scripts/Move.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Move : MonoBehaviour
+{
+    // Start is called before the first frame update
+    [SerializeField] float speed = 5;
+    [SerializeField] Camera camera;
+    private float width, height;
+    private float objectWidth, objectHeight;
+
+
+    void Start()
+    {
+        height = 2f * camera.orthographicSize /2;
+        width = height * camera.aspect;
+        objectWidth = transform.GetComponent<SpriteRenderer>().bounds.size.x / 2;
+        objectHeight = transform.GetComponent<SpriteRenderer>().bounds.size.y / 2;
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector2 vec = transform.position;
+        float x = transform.position.x + (speed * Time.deltaTime) * Input.GetAxis("Horizontal"); //move right or left or not
+        float y = transform.position.y + ( speed * Time.deltaTime) * Input.GetAxis("Vertical"); //move up or down or not
+        
+        
+        //prevents the player from exceeding the boarder
+        x = Mathf.Clamp(x, width*-1+ objectWidth, width- objectWidth);
+        y = Mathf.Clamp(y, height*-1+ objectHeight, height- objectHeight);
+
+        transform.position = new Vector3(x,y,0);
+    }
+}
---
+++ Lesson9/New Unity Project/Assets/Scripts/Rope.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rope : MonoBehaviour
+{
+    [SerializeField] GameObject Baloon;
+    [SerializeField] GameObject Player;
+    [SerializeField] float offsetx;
+    [SerializeField] float offsety;
+
+    private LineRenderer line;                           // Line Renderer
+
+    // Use this for initialization
+    void Start()
+    {
+        // Add a Line Renderer to the GameObject
+        line = this.gameObject.AddComponent<LineRenderer>();
+        // Set the width of the Line Renderer
+        line.SetWidth(0.05F, 0.05F);
+        // Set the number of vertex fo the Line Renderer
+        line.SetVertexCount(2);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // Check if the GameObjects are not null
+        if (Baloon != null && Player != null)
+        {
+            // Update position of the two vertex of the Line Renderer
+            line.SetColors(Color.black,Color.black);
+            Vector3 vec = Baloon.transform.position;
+            vec.z += .5f;
+            line.SetPosition(0, vec);
+           vec = Player.transform.position;
+           vec.x +=offsetx;
+            vec.y += offsety;
+            vec.z = -0.5f;
+            line.SetPosition(1, vec);
+        }
+    }
+
+}
