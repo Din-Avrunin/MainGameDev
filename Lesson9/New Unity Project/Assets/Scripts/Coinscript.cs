1d1df37382bce6deb0774532e253638be40cb01e
dobzik <dobzik@hotmail.com>
2020-06-14 22:52:26 +0300

    score system, coins added
    Updated coins and score system

---
+++ Lesson9/New Unity Project/Assets/Scripts/Coinscript.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SocialPlatforms.Impl;
+
+public class Coinscript : MonoBehaviour
+{
+    [SerializeField] ScoreManager sm;
+    public void Start()
+    {
+       
+    }
+
+
+    public void Update()
+    {
+        
+    }
+
+    public void OnCollisionEnter2D(Collision2D other)
+    {
+        string otherTag= other.gameObject.tag;
+        if (otherTag == "Player") // if a player touches the gold coin, add to the score the number of points, and destroy the coin
+        {
+
+            sm.updateScore();
+            Debug.Log("tetstdsftgdgdfgd");
+            Destroy(this.gameObject);
+      
+        }
+        else if (otherTag == "Enemy")
+        {
+            Destroy(this.gameObject);
+        }
+            
+    }
+}
---
+++ Lesson9/New Unity Project/Assets/Scripts/ScoreManager.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ScoreManager : MonoBehaviour
+{
+    Text text;
+    [SerializeField] static float score = 0;
+    [SerializeField] float realScore = 0;
+    [SerializeField] int points = 100;
+    [SerializeField] float timeBetweenScore = 1;
+    // Start is called before the first frame update
+    void Start()
+    {
+        text = gameObject.GetComponent<Text>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+       score += Time.deltaTime; // gets a coin every frame 
+        realScore = (int)score;
+        text.text = "score " + realScore;
+        
+
+    }
+    public void updateScore()
+    {
+        score += points;
+    }
+   
+}
