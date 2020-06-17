09a0f67e23ab7eafa61e02ef4bb0bff0d006beb3
Denis <aripash@gmail.com>
2020-06-17 22:54:18 +0300

    add level ui

---
+++ Lesson9/New Unity Project/Assets/Scripts/LevelText.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class LevelText : MonoBehaviour
+{
+    Text Text;
+    // Start is called before the first frame update
+    void Start()
+    {
+        Text = gameObject.GetComponent<Text>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Text.text = "Level " + (SceneManager.GetActiveScene().buildIndex-1);
+    }
+}
